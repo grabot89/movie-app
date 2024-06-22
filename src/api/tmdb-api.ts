6b5f6e9b94293868134c6ea647f6dae2f900c345
Grainne Moran <20104297@mail.wit.ie>
2024-06-22 14:22:35 +0100

    Full review page.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -29,6 +29,7 @@ return fetch(
     .then((res) => res.json())
     .then((json) => json.posters);
 };
+
 export const getMovieReviews = (id: string | number) => { //movie id can be string or number
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
@@ -38,4 +39,12 @@ export const getMovieReviews = (id: string | number) => { //movie id can be stri
         // console.log(json.results);
         return json.results;
       });
-  };
+};
+
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+  )
+    .then(res => res.json())
+    .then(json => json.results);
+};
