9bb3b998e3691cb6eb5447d3684b32f4f6356b63
Grainne Moran <20104297@mail.wit.ie>
2024-06-23 21:05:57 +0100

    Add new review support.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -68,3 +68,10 @@ export interface DiscoverMovies {
   total_results: number;
   results: BaseMovieProps[];
 }
+export interface Review {
+  author: string,
+  content: string,
+  agree: boolean,
+  rating: number,
+  movieId: number,
+}
